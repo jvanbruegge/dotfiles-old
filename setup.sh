91a7d9b7785da23b09ba5f7b27d21cd3ad33ac17
Jan van Brügge <supermanitu@gmail.com>
2019-04-29 12:35:48 +0200

    Add more aliases

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-pacaur -S rustup nvim python3-pip
+yay -S rustup nvim python3-pip
 pip3 install neovim
 
 # Rust compiler + Language server
 rustup toolchain install stable
 rustup default stable
 
-rustup component add rls-preview
+rustup component add rls
 rustup component add rust-analysis
 rustup component add rust-src
 
 # Typescript compiler + Language server
-pacaur -S npm nodejs
+yay -S npm nodejs
 sudo npm i -g typescript-language-server
 
 # C compiler + Language server
-pacaur -S clang clang-tools-extra
+yay -S clang clang-tools-extra
 
 # Haskell toolchain + Language server
-pacaur -S stack
+yay -S stack
 git clone https://github.com/haskell/haskell-ide-engine
 cd haskell-ide-engine
 stack install
@@ -27,7 +27,7 @@ cd ..
 rm -rf haskell-ide-engine
 
 # Java language server
-pacaur -S jdtls
+yay -S jdtls
 
 # C# compiler and language server
-pacaur -S omnisharp-roslyn
+yay -S omnisharp-roslyn
